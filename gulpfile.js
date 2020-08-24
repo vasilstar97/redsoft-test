7fd367997bae71ab491693d80a9ec1f2891267ae
Vasiliy <vasilstar97@gmail.com>
2020-08-24 15:07:38 +0500

    Gulpfile changed to work with images etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const { task } = require('gulp');
+
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create(),
@@ -8,27 +10,40 @@ var srcDir = './src';
 var buildDir = './build';
 
 gulp.task('default', function () {
-    OnSass();
+    gulp.parallel('scss', 'img', 'js', 'fonts')();
+
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch(srcDir + "/scss/**/*", gulp.series('sass'));
+    gulp.watch(srcDir + "/scss/**/*", gulp.series('scss'));
+    gulp.watch(srcDir + "/img/**/*", gulp.series('img'));
+    gulp.watch(srcDir + "/js/**/*", gulp.series('js'));
     gulp.watch("index.html").on('change', browserSync.reload);
 });
 
-gulp.task('sass', function () {
-    return OnSass();
-});
-
-function OnSass() {
+gulp.task('scss', function () {
     return gulp.src(srcDir + '/scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 4 versions'],
             cascade: true
         }))
         .pipe(gulp.dest(buildDir + '/css'))
         .pipe(browserSync.stream());
-}
+});
+
+gulp.task('img', function () {
+    return gulp.src(srcDir + '/img/**/*')
+        .pipe(gulp.dest(buildDir + '/img'));
+});
+
+gulp.task('js', function () {
+    return gulp.src(srcDir + '/js/**/*')
+        .pipe(gulp.dest(buildDir + '/js'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src(srcDir + '/fonts/**/*')
+        .pipe(gulp.dest(buildDir + '/fonts'));
+});
