a231f7a5e496461459d8167a1521e6de73b16c2c
Vasiliy <vasilstar97@gmail.com>
2020-08-25 13:24:50 +0500

    button toggling with fetch

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,38 +1,61 @@
 function refreshStorage() {
     let json = {};
     let domGalleryItems = document.querySelectorAll('.main__item.item[data-id]:not(.item_unactive)');
-    domGalleryItems.forEach(element => {
+    for (var i = 0; i < domGalleryItems.length; ++i) {
+        let element = domGalleryItems[i];
         let id = element.attributes['data-id'].value;
         let value = element.querySelector('.button.button_in-cart') != null;
         json[id] = value;
-    });
+    }
+    // domGalleryItems.forEach(element => {
+    //     let id = element.attributes['data-id'].value;
+    //     let value = element.querySelector('.button.button_in-cart') != null;
+    //     json[id] = value;
+    // });
     sessionStorage.setItem('redsoft-gallery', JSON.stringify(json));
 }
 
-function toggleButton(button, doRefresh) {
+function toggleButton(button, cls, doRefresh) {
     if (button.classList) {
-        button.classList.toggle("button_in-cart");
+        button.classList.toggle(cls);
+        if (button.classList.contains(cls))
+            button.innerHTML = "В корзине";
+        else button.innerHTML = "Купить";
     } else {
         // For IE9
         var classes = button.className.split(" ");
-        var i = classes.indexOf("button_in-cart");
+        var i = classes.indexOf(cls);
 
         if (i >= 0)
             classes.splice(i, 1);
         else
-            classes.push("button_in-cart");
+            classes.push(cls);
         element.className = classes.join(" ");
     }
     if (doRefresh) refreshStorage();
 }
 
+
 function addToggleButtonEvents() {
-    document.querySelectorAll('.main__item.item[data-id] .button').forEach(element => {
-        console.log(element);
-        element.addEventListener('click', function () {
-            toggleButton(element, true);
-        });
-    });
+    let buttons = document.querySelectorAll('.main__item.item[data-id] .button');
+    for (let i = 0; i < buttons.length; i++) {
+        const element = buttons[i];
+        element.onclick = async function () {
+            if (element.classList.contains("button_is-loading")) return;
+            if (element.classList.contains("button_in-cart")) {
+                toggleButton(element, 'button_in-cart', true);
+                return;
+            }
+
+            let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + (i + 1));
+            toggleButton(element, 'button_is-loading', false);
+            if (response.ok) {
+                let json = await response.json();
+                toggleButton(element, 'button_in-cart', true);
+                toggleButton(element, 'button_is-loading', false);
+            }
+        };
+    }
 }
 
 let galleryItems = sessionStorage.getItem('redsoft-gallery');
@@ -43,8 +66,7 @@ if (galleryItems == null) {
     for (var key in json) {
         let item = document.querySelector('.main__item.item[data-id="' + key + '"]');
         let button = item.querySelector('.button');
-        if (json[key]) toggleButton(button, false);
-        console.log(json[key]);
+        if (json[key]) toggleButton(button, 'button_in-cart', false);
     }
 }
 addToggleButtonEvents();
